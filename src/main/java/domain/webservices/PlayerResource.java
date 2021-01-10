10118a6081e46fa721bf8db0126b09df34a2c3fc
Michel <m.kwantes@hotmail.com>
2021-01-10 23:03:10 +0100

    updated version

--- src/main/java/domain/webservices/PlayerResource.java
+++ src/main/java/domain/webservices/PlayerResource.java
@@ -47,8 +47,6 @@ public class PlayerResource {
     @DELETE
     @Path("/{id}")
     public void deletePlayer(@PathParam("id") String playerName) {
-        System.out.println(SaveFile.getSaveFile().getPlayerList());
         SaveFile.getSaveFile().removePlayer(playerName);
-        System.out.println("lol hier komt die wel hehehhe");
     }
 }
