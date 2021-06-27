df83a1360a8f1ecd89ade5633648388e701bf6e7
Michel <m.kwantes@hotmail.com>
2021-06-27 16:10:37 +0200

    attempting online deployment

--- src/test/java/domain/model/MonsterManagerTest.java
+++ src/test/java/domain/model/MonsterManagerTest.java
@@ -1,11 +1,10 @@
 package domain.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import domain.model.Monster;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -13,25 +12,37 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MonsterManagerTest {
     private ArrayList<Monster> monsterList = new ArrayList<>();
 
-    @BeforeAll
-    void intializeTest() {
-        ObjectMapper mapper = new ObjectMapper();
-        List<Monster> monsterList = null;
+    public void setMonsterList(ArrayList<Monster> monsterList) {
+        this.monsterList = monsterList;
+    }
 
+    public ArrayList<Monster> getMonsterList() {
+        return monsterList;
+    }
+
+    @BeforeEach
+    public void init() {
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayList<Monster> monsterList = new ArrayList<>();
         try {
             monsterList = mapper.readValue(new URL("https://dl.dropboxusercontent.com/s/iwz112i0bxp2n4a/5e-SRD-Monsters.json"), new TypeReference<List<Monster>>() {
             });
+            setMonsterList(monsterList);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        MonsterManager.setAllMonsters(new ArrayList<Monster>(monsterList));
+
     }
 
     @Test
+    @DisplayName("testing getMonster method")
     void testGetMonster() {
-        
+        Monster monster = MonsterManager.getMonster("Imp");
+        assert monster != null;
     }
 }
